5e55e36333d85303decbe487c12fdb3311625fd4
zzxkll <149194735+zzxkll@users.noreply.github.com>
2024-10-30 11:07:16 +0000

    няняня

--- task2.py
+++ task2.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy import sqrt 
-from constans import k , g 
+from constans import k , g , e
 h = 100 
 a = 45
 b = 35
@@ -8,8 +8,11 @@ b = 35
 
 V = sqrt((g* h * np.tan(b)**2)/(2 * np.cos(a)**2 * (1- np.tan(b) * np.tan(a) )))
 
-print(V)
+print('V =' , V)
 
 T = 200
+e1 = 300
 
-N = (2 / sqrt(np.pi)) * (h/ (k * T)**(3/2))
+N = (2 / sqrt(np.pi)) * (h/ (k * T)**(3/2)) * e**(-e1/k*T) * e1**(T/2)
+
+print('N =', N)
